df43b002fe2f7a10bb822927c7f6ac64bede74d8
noamarchante <noamarchante@gmail.com>
2021-04-11 19:52:05 +0200

    Adds event repository
    Adds basic event repository file

---
+++ src/main/java/esei/tfg/apachas/repository/REvent.java
@@ -0,0 +1,18 @@
+package esei.tfg.apachas.repository;
+
+
+import esei.tfg.apachas.entity.Event;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository("REvent")
+public interface REvent extends CrudRepository<Event, Long>, PagingAndSortingRepository<Event, Long> {
+
+    Event findByEventId(long eventId);
+
+    @Override
+    Page<Event> findAll(Pageable pageable);
+}
