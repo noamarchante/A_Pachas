47191601ad611cc5c58536e7cf93b4124690e09c
noamarchante <noamarchante@gmail.com>
2021-04-11 10:42:56 +0200

    Adds basic project
    Adds pom, properties, main and test main files

---
+++ src/main/java/esei/tfg/apachas/ApachasApplication.java
@@ -0,0 +1,13 @@
+package esei.tfg.apachas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApachasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApachasApplication.class, args);
+    }
+
+}
---
+++ src/test/java/esei/tfg/apachas/ApachasApplicationTests.java
@@ -0,0 +1,13 @@
+package esei.tfg.apachas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApachasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
