316897e6fe35091627fa5690cc864100260cc614
noamarchante <noamarchante@gmail.com>
2021-04-11 20:39:46 +0200

    Adds usergroupuser converter
    Adds usergroupuser converter file

---
+++ src/main/java/esei/tfg/apachas/converter/ConUserGroupUser.java
@@ -0,0 +1,27 @@
+package esei.tfg.apachas.converter;
+
+import org.springframework.stereotype.Component;
+import esei.tfg.apachas.entity.UserGroupUser;
+import esei.tfg.apachas.entity.id.UserGroupUserId;
+import esei.tfg.apachas.model.MUserGroupUser;
+import java.util.ArrayList;
+import java.util.List;
+
+@Component("ConUserGroupUser")
+public class ConUserGroupUser {
+    public List<MUserGroupUser> conUserGroupUserList(List<UserGroupUser> userGroupUserList) {
+        List<MUserGroupUser> mUserGroupUserList = new ArrayList<>();
+        for (UserGroupUser userGroupUser : userGroupUserList) {
+            mUserGroupUserList.add(new MUserGroupUser(userGroupUser));
+        }
+        return mUserGroupUserList;
+    }
+
+    public MUserGroupUser conUserGroupUser(UserGroupUser userGroupUser) {
+        return new MUserGroupUser(userGroupUser);
+    }
+
+    public UserGroupUser conMUserGroupUser(MUserGroupUser mUserGroupUser) {
+        return new UserGroupUser(new UserGroupUserId(mUserGroupUser.getUserGroupId(), mUserGroupUser.getUserId()));
+    }
+}
